90d8b2c40aa36c6381791aa12cab5a94ce14c689
Dasom Kim <llsomll90@gmail.com>
2024-07-09 14:04:39 -0400

    second commit

--- main.js
+++ main.js
@@ -2,7 +2,7 @@
 let news = [];
 
 const getLatestNews = async ()=>{
-    // const url = new URL(`https://newsapi.org/v2/top-headlines?country=us&apiKey=${API_KEY}`);
+    // const url = new URL(`https://legendary-faun-d73167.netlify.app/top-headlines?country=us&apiKey=${API_KEY}`);
     const url = new URL(`http://times-node-env.eba-appvq3ef.ap-northeast-2.elasticbeanstalk.com/top-headlines`);
     const response = await fetch(url);
     const data = await response.json();

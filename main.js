cb810331c8bbe660f8085c69a361c3abfef25c5b
Dasom Kim <llsomll90@gmail.com>
2024-07-09 14:11:34 -0400

    third commit

--- main.js
+++ main.js
@@ -2,8 +2,8 @@
 let news = [];
 
 const getLatestNews = async ()=>{
-    // const url = new URL(`https://legendary-faun-d73167.netlify.app/top-headlines?country=us&apiKey=${API_KEY}`);
-    const url = new URL(`http://times-node-env.eba-appvq3ef.ap-northeast-2.elasticbeanstalk.com/top-headlines`);
+    // const url = new URL(`http://times-node-env.eba-appvq3ef.ap-northeast-2.elasticbeanstalk.com/top-headlines?country=us&apiKey=${API_KEY}`);
+    const url = new URL(`https://legendary-faun-d73167.netlify.app/top-headlines`);
     const response = await fetch(url);
     const data = await response.json();
     news = data.articles;

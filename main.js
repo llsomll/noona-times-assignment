9a50209aea947cbb5a35d0ad1a2d7af9e28b4af9
Dasom Kim <llsomll90@gmail.com>
2024-07-10 11:42:35 -0400

    second assignment

--- main.js
+++ main.js
@@ -1,13 +1,58 @@
-// const API_KEY = `17d92ecdea1142fa964e877d98fa9512`;
-let news = [];
+const API_KEY = `17d92ecdea1142fa964e877d98fa9512`;
+let newsList = [];
+const noPhoto = "images/no-photo.jpg";
 
-const getLatestNews = async ()=>{
-    // const url = new URL(`http://times-node-env.eba-appvq3ef.ap-northeast-2.elasticbeanstalk.com/top-headlines?country=us&apiKey=${API_KEY}`);
-    const url = new URL(`https://legendary-faun-d73167.netlify.app/top-headlines`);
+const getLatestNews = async () => {
+    const url = new URL(`https://noona-times-be-5ca9402f90d9.herokuapp.com/top-headlines`);
     const response = await fetch(url);
     const data = await response.json();
-    news = data.articles;
-    console.log("dddd", news);
+    newsList = data.articles;
+    render();
+    console.log("dddd", newsList);
 };
 
+const render = () => {
+    const newsHTML = newsList.map(news=>`<div class="row news">
+          <div class="col-lg-4">
+            <img
+              class="news-img-size"
+              src="${news.urlToImage == null ? noPhoto : news.urlToImage}"
+            />
+          </div>
+          <div class="col-lg-8">
+            <h2>${news.title}</h2>
+            <p>${
+                news.description == null || news.description == ""
+                  ? "No Content"
+                  : news.description.length > 200
+                  ? news.description.substring(0, 200) + "..."
+                  : news.description
+              }</p>
+            <div>${news.source.name == null?"no source":news.source.name} * ${moment(news.publishedAt).fromNow()}</div>
+          </div>
+        </div>`).join('');
+
+
+    document.getElementById('news-board').innerHTML = newsHTML;
+}
+
+
 getLatestNews();
+
+
+const openNav = () => {
+    document.getElementById("mySidenav").style.width = "250px";
+};
+
+const closeNav = () => {
+    document.getElementById("mySidenav").style.width = "0";
+};
+
+const openSearchBox = () => {
+    let inputArea = document.getElementById("input-area");
+    if (inputArea.style.display === "inline") {
+        inputArea.style.display = "none";
+    } else {
+        inputArea.style.display = "inline";
+    }
+};
